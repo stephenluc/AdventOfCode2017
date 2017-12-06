854e74020b9345a8913ba1c9eafb28b685984e62
stephenluc <stephenluc96@gmail.com>
2017-12-06 00:55:44 -0500

    added in input files for previous days

--- day3.py
+++ day3.py
@@ -5,7 +5,8 @@ def spiral_memory(square):
     bottom_right_steps = bottom_right_steps if bottom_right_steps % 2 != 0 else bottom_right_steps + 1
     print bottom_right_steps - (pow(bottom_right_steps, 2) - square) % (bottom_right_steps - 1) - 1
 
-square = 265149
+f = open('03_input.txt', 'r')
+square = int(f.readline().strip())
 spiral_memory(square)
 
     
