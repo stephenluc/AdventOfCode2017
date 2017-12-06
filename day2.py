0bce5e38230880dbaaf6e9e6a1236cc00226007b
stephenluc <stephenluc96@gmail.com>
2017-12-06 15:51:22 -0500

    finished part 2 for day 2 and 4

--- day2.py
+++ day2.py
@@ -4,9 +4,23 @@ def corruption_checksum(spreadsheet):
         sum += max(row) - min(row)
     print sum
 
+def part_2(spreadsheet):
+    sum = 0
+    for row in spreadsheet:
+        for number in row:
+            if [x % number for x in row].count(0) > 1:
+                row.remove(number)
+                sum += [x / number for x in row if x % number == 0][0]
+                break
+    print sum
+
 f = open('02_input.txt', 'r')
 spreadsheet = []
 for line in f.readlines():
     spreadsheet.append(map(int, line.strip().split('\t')))
 
-corruption_checksum(spreadsheet)
+# Part 1
+# corruption_checksum(spreadsheet)
+
+# Part 2
+# part_2(spreadsheet)
--- day4.py
+++ day4.py
@@ -9,7 +9,22 @@ def high_entropy_passphrases(passphrases):
             if phrase[i] == phrase[i + 1]:
                 valid = False
             i += 1
+        if valid:
+            valid_phrases += 1
+    print valid_phrases
 
+def part_2(passphrases):
+    valid_phrases = 0
+    for phrase in passphrases:
+        phrase = phrase.strip().split(' ')
+        phrase = [''.join(sorted(x)) for x in phrase]
+        phrase.sort()
+        i = 0
+        valid = True
+        while i < (len(phrase) - 1) and valid:
+            if phrase[i] == phrase[i + 1]:
+                valid = False
+            i += 1
         if valid:
             valid_phrases += 1
     print valid_phrases
@@ -17,4 +32,8 @@ def high_entropy_passphrases(passphrases):
 f = open('04_input.txt', 'r')
 passphrases = f.readlines()
 
-high_entropy_passphrases(passphrases)
+# Part 1
+# high_entropy_passphrases(passphrases)
+
+# Part 2
+# part_2(passphrases)
